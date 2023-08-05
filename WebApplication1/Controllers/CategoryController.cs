9df28b43dd92e0cbd57f706edde5bf7c949f5281
Sourav Paul <sourav.paul.ext@flydubai.com>
2023-08-06 03:04:32 +0530

    new changes

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -16,5 +16,25 @@ namespace WebApplication1.Controllers
             List<Category> ObjCategoryList = _db.Categories.ToList();
             return View(ObjCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
