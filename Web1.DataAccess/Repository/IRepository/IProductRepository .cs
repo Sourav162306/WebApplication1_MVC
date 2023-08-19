a521ade749938d7267dbc812051c52a9e2936d92
Sourav Paul <sourav.paul.ext@flydubai.com>
2023-08-20 02:24:35 +0530

    new file and product included

---
+++ Web1.DataAccess/Repository/IRepository/IProductRepository .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web1.Models;
+
+namespace Web1.DataAccess.Repository.IRepository
+{
+    public interface IproductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
---
+++ Web1.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web1.DataAccess.Data;
+using Web1.DataAccess.Repository.IRepository;
+using Web1.Models;
+
+namespace Web1.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+
+        public IproductRepository Product { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
+        }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
